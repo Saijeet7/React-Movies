d359c84900f47a5ddea3c195d2ec46e4dd257f38
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-10-28 15:22:54 +0545

    Added: Grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+
 // Hook
 import { useHomeFetch} from '../hooks/useHomeFetch';
 // Image
@@ -21,8 +23,12 @@ const Home = () => {
                 title={state.results[0].original_title}
                 text={state.results[0].overview}
                 />
-             ) : null
-        }
+             ) : null}
+             <Grid header='Popular Movies'>
+                 {state?.results.map(movie => (
+                     <div key={movie.id}>{movie.title}</div>
+                 ))}
+             </Grid>
         </>
     );
 };
