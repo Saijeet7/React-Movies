1718f8ea93fbe9ff55ad928739cd846f2c560be8
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-12-21 22:08:43 +0545

    Complete : deploy ready

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,30 +1,31 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    background: var(--darkGrey);
-    padding: 0 20px;
+  background: var(--darkGrey);
+  padding: 0 20px;
 `;
 
 export const Content = styled.div`
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   max-width: var(--maxWidth);
-   padding: 20px 0;
-   margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
+  svg {
+    width: 100px;
+  }
+  @media screen and (max-width: 500px) {
+    width: 80px;
+  }
 `;
 
-export const LogoImg = styled.img`
-     width: 200px;
+export const LogoImg = styled.div`
+  svg {
+    width: 200px;
 
-     @media screen and (max-width:500px){
-         width: 150px;
-     }
-`;
-
-export const TMDBLogoImg = styled.img`
-     width: 100px;
-     @media screen and (max-width:500px){
-         width: 80px;
-     }
+    @media screen and (max-width: 500px) {
+      width: 150px;
+    }
+  }
 `;
